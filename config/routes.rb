7402c01d5c9f202b33e0c3e4276e0114fcc955f7
Valentin Šarić <valentin.saric1@gmail.com>
2019-07-04 00:02:03 +0200

    Added admin functionalityes and view for admin settings

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,18 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :posts
-
+  resources :admins do
+    member do
+      patch :set
+      put :set
+      patch :remove
+      put :remove
+      patch :set
+      put :set
+      patch :blocked
+      put :blocked
+    end
+  end
   root "posts#index"
 
 end
